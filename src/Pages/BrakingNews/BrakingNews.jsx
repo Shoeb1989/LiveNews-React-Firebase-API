31320325608d7501919159225ad7f87ee82a605a
Shoeb1989 <shoebsikder1@gmail.com>
2024-09-28 22:32:50 +0600

    Done

--- src/Pages/BrakingNews/BrakingNews.jsx
+++ src/Pages/BrakingNews/BrakingNews.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Marquee from "react-fast-marquee";
+import img from '../../pictures/braking.png'
 
 const BrakingNews = () => {
     return (
         <div className='flex border border-spacing-2 mt-4 ' >
             <button className='btn btn-secondary' >
-                Braking News
+            <img className='mx-auto object-cover h-10 w-40 bg-indigo-600 bg-opacity-0 ' src={img} alt="" />
             </button>
             <Marquee pauseOnHover={true} speed={80} >
                Braking News Comming Soon.... <span className='text-blue-950 font-bold' >Login to Read Full news</span>
--- src/Pages/Header/Header.jsx
+++ src/Pages/Header/Header.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from "../../pictures/logo.png"
 import moment from 'moment';
+import pic from '../../pictures/News_Live.webp'
 
 
 const Header = () => {
     return (
         <div className='text-center mt-5 ' >
-            <img className='mx-auto' src={logo} alt="" />
+            <img className='mx-auto object-fill h-28 w-76  ' src={pic} alt="" /> 
+            
             <p className='mt-4' >Jounalism Without Fear or favor</p>
             <p className='text-xl mt-4 '>{moment().format("dddd, MMMM D, YYYY")}</p>
         </div>
